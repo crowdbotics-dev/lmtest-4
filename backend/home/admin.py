8e877f9653ecdf94ee32321b723a10e043a39e17
crowdbotics-dev <engineering@crowdbotics.com>
2023-12-07 18:14:39 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cxdsf
+
+admin.site.register(Cxdsf)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Cxdsf(models.Model):
+    "Generated Model"
+    asfsfs = models.BigIntegerField()
